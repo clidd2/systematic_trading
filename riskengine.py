23f7973ecfb2fee90fc3fcffd563146bb745ff5a
clidd2 <31219768+clidd2@users.noreply.github.com>
2022-09-05 00:04:09 -0400

    Took data collection function from risk engine and placed in util file

--- riskengine.py
+++ riskengine.py
@@ -1,21 +1,7 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
-import pandas_datareader.data as web
-
-def get_data(ticker, start_date, end_date=dt.datetime.today(), colname='Adj Close'):
-    try:
-        #frame containing price data for given security in given date range
-        df = web.DataReader(name=ticker,data_source='yahoo',start=start_date,
-                            end=end_date)
-
-        #convert index from string to datetime
-        df.index = pd.to_datetime(df.index)
-
-    except Exception as err:
-        print(err)
-
-    return df.rename(columns={colname:'price'}).reset_index()
+from utils import get_data
 
 
 def average_trading_range(df, periods=20):
